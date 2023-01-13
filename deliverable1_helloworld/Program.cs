3630b4745150151558b34b9c09b1d30d7a5d66d3
samee <samee@LAPTOP-2MQK9SK0>
2023-01-12 20:17:03 -0500

    Add project files.

---
+++ deliverable1_helloworld/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Transactions;
+
+namespace deliverable1_helloworld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Solution (1): Hello, World!");
+            Console.WriteLine(" ");
+
+            string stg = "Hello, World!";
+            Console.WriteLine("Solution (2):" + stg);
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Enter a username: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("The user's name is: " + userName);
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Enter user's age: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("User's age is: " + age);
+
+        }
+    }
+}
